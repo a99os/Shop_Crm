6da67d27e86376bc5e746b522e20b92826381df1
a99os <abbosashirboev@gmail.com>
2022-12-06 12:55:30 +0500

    add subCategies model

---
+++ src/sub-categories/sub-category.model.ts
@@ -0,0 +1,38 @@
+import {
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { Categories } from '../categories/categories.model';
+
+interface SubCategoryCreateAttr {
+  category_id: number;
+  sub_category_name: string;
+}
+
+@Table({ tableName: 'sub_categories' })
+export class SubCategory extends Model<SubCategory, SubCategoryCreateAttr> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  sub_category_id: number;
+
+  @ForeignKey(() => Categories)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  category_id: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  sub_category_name: string;
+}
