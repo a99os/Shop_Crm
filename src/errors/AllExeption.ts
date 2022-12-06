6adb4cfa9c175fb32b6ad5993b9495df506b4d9b
a99os <abbosashirboev@gmail.com>
2022-12-06 12:33:14 +0500

    add AllExeptionFilter

---
+++ src/errors/AllExeption.ts
@@ -0,0 +1,37 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+interface Iexep {
+  [key: string]: string;
+}
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: Iexep, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    console.log(exception);
+    const responseBody = {
+      message: exception.response || exception.errors,
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './errors/AllExeption';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3030;
 
   const app = await NestFactory.create(AppModule);
+
+  const adapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new AllExceptionsFilter(adapterHost));
   await app.listen(PORT, () => {
     console.log('Listen app port-->>' + PORT);
   });
